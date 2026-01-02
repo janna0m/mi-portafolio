a2958a54ce6855adb4e4269e36927599fd9cfef7
Jana <jana@MacBook-Air-de-Jana.local>
2026-01-03 00:51:09 +0100

    update

--- ANALOGICO0/css_menu_responsive.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";//además de ser de la clase topnav, también lo va a ser de responsive
-  } else {
-    x.className = "topnav";//vuelve a ser solo de la clase topnav
-  }
-}
